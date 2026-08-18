29a3781fba24e15c8a8d143e87b517193ecd02d3
Rodolfo Pereira <rolffcoelho@gmail.com>
2026-08-18 18:02:53 +0100

    Fix Phase 1 lint findings

--- src/helix_support_intelligence/data/__init__.py
+++ src/helix_support_intelligence/data/__init__.py
@@ -3,4 +3,4 @@
 from helix_support_intelligence.data.banking77 import Banking77Spec
 from helix_support_intelligence.data.helixbank import CORPUS_VERSION
 
-__all__ = ["Banking77Spec", "CORPUS_VERSION"]
+__all__ = ["CORPUS_VERSION", "Banking77Spec"]
