1064663d1e5957f8f3f8896f0d3e5bfa7a6ae341
Rodolfo Pereira <rolffcoelho@gmail.com>
2026-08-18 23:46:14 +0100

    Expose public routing domain contracts

--- src/helix_support_intelligence/domain/__init__.py
+++ src/helix_support_intelligence/domain/__init__.py
@@ -1,5 +1,29 @@
 """Stable public domain contracts."""
 
 from helix_support_intelligence.domain.decisions import TerminalDecision
+from helix_support_intelligence.domain.routing import (
+    ROUTE_ENDPOINT_METHOD,
+    ROUTE_ENDPOINT_PATH,
+    IntentScorer,
+    RouteAlternative,
+    RouteEndpoint,
+    RouteRequest,
+    RoutingContractError,
+    RoutingDecision,
+    RoutingPolicyConfig,
+    Urgency,
+)
 
-__all__ = ["TerminalDecision"]
+__all__ = [
+    "ROUTE_ENDPOINT_METHOD",
+    "ROUTE_ENDPOINT_PATH",
+    "IntentScorer",
+    "RouteAlternative",
+    "RouteEndpoint",
+    "RouteRequest",
+    "RoutingContractError",
+    "RoutingDecision",
+    "RoutingPolicyConfig",
+    "TerminalDecision",
+    "Urgency",
+]
